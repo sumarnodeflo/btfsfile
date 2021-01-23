139a87a81c7b7cc9e17fce9b364c4162213f71fc
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-01-23 12:58:20 +0700

    Create installbtfsonuser.sh

---
+++ installbtfsonuser.sh
@@ -0,0 +1,33 @@
+#!bash/sh
+w='\e[97m'
+g='\033[1;92m'
+r='\033[1;91m'
+a='\033[1;94m'
+b='\e[1;4m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+p='\033[0m' #putih
+
+echo "$green Script Bios13 Mode On........$p"
+sleep 2
+wget https://raw.githubusercontent.com/TRON-US/btfs-binary-releases/master/install.sh
+echo "$Script User Tamvan$p"
+sleep 2
+bash install.sh -o linux -a amd64
+sleep 2
+export PATH=${PATH}:${HOME}/btfs/bin
+sleep 2
+btfs init
+
+echo "$lanjut Run Daemon$p"
+sleep 2
+btfs daemon
+
+echo "$yellow Install BTFS Step 1 Selesai$p"
+sleep 3
+
+exit
