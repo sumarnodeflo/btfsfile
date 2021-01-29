081bd1ec3a2e472b60392826eaa80b5ec37526bf
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-01-29 22:15:55 +0700

    Create btfsdaemon-monitor.sh

---
+++ btfsdaemon-monitor.sh
@@ -0,0 +1,15 @@
+# vi /opt/scripts/btfsdaemon-monitor.sh
+
+#!/bin/bash
+serv=btfs daemon
+sstat=stopped
+service $serv status | grep -i 'running\|stopped' | awk '{print $3}' | while read output;
+do
+echo $output
+if [ "$output" == "$sstat" ]; then
+    service $serv start
+    echo "$serv service is UP now.!" 
+    else
+    echo "$serv service is running"
+    fi
+done
