17c1eb5e3dea5ce1ef082fcff79e3d3a6e752428
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-01-30 21:30:53 +0700

    Create autoscp.sh

---
+++ autoscp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#!/usr/bin/expect -f
+
+# connect via scp
+spawn scp -P 22 btfs-DO1NY.zip root@178.128.154.129:/root/.
+#######################
+expect {
+-re ".*es.*o.*" {
+    exp_send "yes\r"
+    exp_continue
+}
+-re ".*sword.*" {
+    exp_send "PASWORDSERVER\r"
+}
+}
+interact
