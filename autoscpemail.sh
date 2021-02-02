2e683c70adc6406016b65b66b0044f4519090b65
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-02 20:57:57 +0700

    Update autoscpemail.sh

--- autoscpemail.sh
+++ autoscpemail.sh
@@ -6,9 +6,9 @@ echo "Backup System On $now" 1>&2
 
 expect -c 'spawn scp -P 22 btfs-DO1NY.zip root@178.128.154.129:/root/.; sleep 10; expect password; send "GANTIPASWORD\n"; interact'
 
-/usr/sbin/sendmail -i -- sumarno.deflo@gmail.com <<EOF
+/usr/sbin/sendmail -i -- thexplor3r@gmail.com <<EOF
 subject: DB BACKUP!!
-from: sumarno.deflo@gmail.com
+from: thexplor3r@gmail.com
 Data sudah berhasil dibackup
 EOF
 
