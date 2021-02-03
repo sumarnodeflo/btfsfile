d7605bbba08717b2254b0d9e3cafe2e68c5a08e4
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-03 19:06:37 +0700

    Create openportrootbtfs.sh

---
+++ openportrootbtfs.sh
@@ -0,0 +1,101 @@
+#!bash/sh
+w='\e[97m'
+g='\033[1;92m'
+r='\033[1;91m'
+a='\033[1;94m'
+b='\e[1;4m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+p='\033[0m' #putih
+
+sleep 2
+echo "$Menjalankan script Open Port$p"
+sleep 2
+ufw allow 5071/tcp
+sleep 2
+ufw allow 5072/tcp
+sleep 2
+ufw allow 5073/tcp
+sleep 2
+ufw allow 5074/tcp
+sleep 2
+ufw allow 5091/tcp
+sleep 2
+ufw allow 5092/tcp
+sleep 2
+ufw allow 5083/tcp 
+sleep 2
+ufw allow 5084/tcp 
+sleep 2
+ufw allow 4004/tcp
+sleep 2
+ufw allow 4005/tcp
+sleep 2
+ufw allow 1235/tcp
+sleep 2
+ufw allow 1236/tcp
+sleep 2
+ufw allow 1247/tcp
+sleep 2
+ufw allow 1248/tcp
+sleep 2
+ufw allow 1239/tcp
+sleep 2
+ufw allow 1241/tcp
+sleep 2
+ufw allow 1242/tcp
+sleep 2
+ufw allow 1245/tcp
+sleep 2
+ufw allow 1251/tcp
+sleep 2
+ufw allow 1252/tcp
+sleep 2
+ufw allow 5102/tcp
+sleep 2
+ufw allow 5103/tcp
+sleep 2
+ufw allow 5114/tcp
+sleep 2
+ufw allow 5115/tcp
+sleep 2
+ufw allow 5107/tcp
+sleep 2
+ufw allow 5108/tcp
+sleep 2
+ufw allow 5110/tcp
+sleep 2
+ufw allow 5111/tcp
+sleep 2
+ufw allow 5120/tcp
+sleep 2
+ufw allow 5121/tcp
+sleep 2
+ufw allow 8084/tcp
+sleep 2
+ufw allow 8085/tcp
+sleep 2
+ufw allow 8096/tcp
+sleep 2
+ufw allow 8097/tcp
+sleep 2
+ufw allow 8088/tcp
+sleep 2
+ufw allow 8099/tcp
+sleep 2
+ufw allow 8090/tcp
+sleep 2
+ufw allow 8091/tcp
+sleep 2
+ufw allow 8092/tcp
+sleep 2
+ufw allow 8093/tcp
+sleep 2
+echo "$ Open Port di root Selesai $p"
+sleep 3
+
+exit
