2efb571065ea94d860d7730bc306495d768bac5a
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-08 19:46:24 +0700

    Update autoscpemail.sh

--- autoscpemail.sh
+++ autoscpemail.sh
@@ -2,7 +2,7 @@
 
 now="$(date +'%y-%m-%d')"
 now2="$(date +'%y%m%d')"
-echo "Backup System On $now" 1>&2
+echo "Backup System script by Sumarno On $now" 1>&2
 
 expect -c 'spawn scp -P 22 btfs-DO1NY.zip root@178.128.154.129:/root/.; sleep 10; expect password; send "GANTIPASWORD\n"; interact'
 
