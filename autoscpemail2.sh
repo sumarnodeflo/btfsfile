8f7bf8d3996934af87de7141f5e3e607c1601040
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-08 19:39:07 +0700

    Create autoscpemail2.sh

---
+++ autoscpemail2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+now="$(date +'%y-%m-%d')"
+now2="$(date +'%y%m%d')"
+echo "Backup System On $now" 1>&2
+
+expect -c 'spawn scp -P 22 btfs-DO1NY.zip root@178.128.154.129:/root/.; sleep 10; expect password; send "GANTIPASWORD\n"; interact'
+
+echo "Data sudah berhasil dibackup" 1>&2
+
+exit 0
