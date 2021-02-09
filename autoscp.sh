c322be3f6562c364c129ddaee126d90b4e478bec
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-09 11:57:54 +0700

    Update autoscp.sh

--- autoscp.sh
+++ autoscp.sh
@@ -4,7 +4,7 @@ now="$(date +'%y-%m-%d')"
 now2="$(date +'%y%m%d')"
 echo "Backup System script by Sumarno On $now" 1>&2
 
-expect -c 'spawn scp -P 22 btfs-DO1NY.zip root@178.128.154.129:/root/.; sleep 10; expect password; send "GANTIPASWORD\n"; interact'
+expect -c 'spawn scp -P 22 GANTINAMAFILE.zip gantinamauser@178.128.154.129:/root/.; sleep 10; expect password; send "GANTIPASWORD\n"; interact'
 
 echo "Data sudah berhasil dibackup" 1>&2
 
