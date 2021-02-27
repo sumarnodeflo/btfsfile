6dc9f32f609e390aa0f85576b08c9f890dbe34b7
sumarnodeflo <52055899+sumarnodeflo@users.noreply.github.com>
2021-02-27 13:49:40 +0700

    Update installbtfsonuser.sh

--- installbtfsonuser.sh
+++ installbtfsonuser.sh
@@ -12,7 +12,7 @@ blue='\033[1;34m'
 purple='\033[1;35m'
 p='\033[0m' #putih
 
-echo "$green Script Bios13 Mode On........$p"
+echo "$install otomatis btfs........$p"
 sleep 2
 wget https://raw.githubusercontent.com/TRON-US/btfs-binary-releases/master/install.sh
 echo "$Script User Tamvan$p"
